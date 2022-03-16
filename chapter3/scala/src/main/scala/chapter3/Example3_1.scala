5d9dc9c649d82b585d53b9dec3f3a7840fec1ea7
oleh_mykolaishyn <owlleg6@gmail.com>
2022-03-16 15:19:50 +0200

    ex3_1

---
+++ chapter3/scala/src/main/scala/chapter3/Example3_1.scala
@@ -0,0 +1,21 @@
+package main.scala.chapter3
+
+import org.apache.spark.sql.functions.avg
+import org.apache.spark.sql.SparkSession
+
+object Example3_1{
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("DataFrameExample")
+      .getOrCreate()
+
+    val dataDF = spark.createDataFrame(Seq(("Brooke", 20), ("Brooke", 25),
+    ("Denny", 31), ("Jules", 30), ("TD", 35))).toDF("name", "age")
+
+    // Group the same names together, aggregate their ages, and compute an average
+    val avgDF = dataDF.groupBy("name").agg(avg("age"))
+
+    avgDF.show()
+  }
+}
